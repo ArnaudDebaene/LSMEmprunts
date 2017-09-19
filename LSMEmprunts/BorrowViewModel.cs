8bae65859973a6644164602c2977ff8b77ab5caf
Arnaud Debaene <arnaud.debaene@acoemgroup.com>
2017-09-19 18:59:27 +0200

    Work on logic in borrow view

--- LSMEmprunts/BorrowViewModel.cs
+++ LSMEmprunts/BorrowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using LSMEmprunts.Data;
 using Mvvm;
+using Mvvm.Commands;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -20,11 +21,15 @@ namespace LSMEmprunts
 
                 _SelectedUserText = value.Name;
                 OnPropertyChanged(nameof(SelectedUserText));
+                ValidateCommand.RaiseCanExecuteChanged();
             }
         }
 
         public BorrowViewModel()
         {
+            ValidateCommand = new DelegateCommand(ValidateCmd, CanValidateCmd);
+            CancelCommand = new DelegateCommand(CancelCmd, CanCancelCmd);
+
             using (var context = ContextFactory.OpenContext())
             {
                 Users = context.Users.ToList();
@@ -72,6 +77,75 @@ namespace LSMEmprunts
         }
 
         public ObservableCollection<Gear> BorrowedGears { get; } = new ObservableCollection<Gear>();
-        
+
+        private string _Comment;
+        public string Comment
+        {
+            get => _Comment;
+            set => SetProperty(ref _Comment, value);
+        }
+
+        private string _SelectedGearId;
+        public string SelectedGearId
+        {
+            get => _SelectedGearId;
+            set
+            {
+                using (var context = ContextFactory.OpenContext())
+                {
+                    var valueLower = value.ToLowerInvariant();
+                    var gearByName = context.Gears.FirstOrDefault(e => e.Name.ToLowerInvariant() == valueLower);
+                    if (gearByName!=null)
+                    {
+                        System.Diagnostics.Debug.WriteLine("Found matching gear by name");
+                        BorrowedGears.Add(gearByName);
+                        SetProperty(ref _SelectedGearId, string.Empty);
+                        return;
+                    }
+
+                    var gearByScan = context.Gears.FirstOrDefault(e => e.BarCode == value);
+                    if (gearByScan!=null)
+                    {
+                        System.Diagnostics.Debug.WriteLine("Found matching gear by scan");
+                        BorrowedGears.Add(gearByScan);
+                        SetProperty(ref _SelectedGearId, string.Empty);
+                        return;
+                    }
+
+                    //if the gear was not found, simply update the typed value
+                    SetProperty(ref _SelectedGearId, value);
+                }
+            }
+        }
+
+        public DelegateCommand ValidateCommand { get; }
+        private void ValidateCmd()
+        {
+            if (!UserSet)
+            {
+                UserSet = true;
+                return;
+            }
+        }
+        private bool CanValidateCmd()
+        {
+            if (!UserSet)
+            {
+                return SelectedUser != null;
+            }
+            else
+            {
+                return BorrowedGears.Any();
+            }
+        }
+
+        public DelegateCommand CancelCommand { get; }
+        private void CancelCmd()
+        {
+        }
+        private bool CanCancelCmd()
+        {
+            return true;
+        }
     }
 }
